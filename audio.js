16d65ce42e51a4cf1db651f263b80e4ef069c1ab
roulendz <rolands.zeltins@inbox.lv>
2023-07-05 01:37:34 +0300

    Refactored audio.js

--- audio.js
+++ audio.js
@@ -12,32 +12,32 @@ if (savedTime) {
 }
 
 // Add event listener for updating the audio time in localStorage
-audio.addEventListener("timeupdate", function () {
+audio.addEventListener("timeupdate", () => {
   localStorage.setItem("audioTime", audio.currentTime);
 });
 
 // Define a function to pause the audio temporarily
-function pauseAudio() {
+const pauseAudio = () => {
   // Clear any previous timeout
   clearTimeout(timer);
   // Pause the audio
   audio.pause();
   // Set a timeout to resume the audio after 500 ms
   timer = setTimeout(resumeAudio, 500);
-}
+};
 
 // Define a function to pause the audio permanently
-function pauseAudioPermanent() {
+const pauseAudioPermanent = () => {
   audio.pause();
-}
+};
 
 // Define a function to resume the audio
-function resumeAudio() {
+const resumeAudio = () => {
   audio.play();
-}
+};
 
 // Get the window object
-let win = window;
+const win = window;
 
 // Add event listeners for window blur and focus events
 win.addEventListener("blur", pauseAudioPermanent); // Pause when window loses focus
@@ -50,10 +50,10 @@ const textarea = document.getElementById("textarea");
 textarea.addEventListener("keydown", pauseAudio); // Pause when typing in textarea
 
 // Define an array of keys that should pause the audio when pressed in textarea
-let keys = ["Backspace", "Alt", "Control", "Space", "Shift", "Delete"];
+const keys = ["Backspace", "Alt", "Control", "Space", "Shift", "Delete"];
 
 // Add event listener for textarea keydown event with specific keys
-textarea.addEventListener("keydown", function (e) {
+textarea.addEventListener("keydown", (e) => {
   if (keys.includes(e.key)) {
     pauseAudio(); // Pause when pressing one of the keys in textarea
   }
